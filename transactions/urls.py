5f1165ba5aae479b732f923e49f068906ca2e792
Leo <leocasti2@gmail.com>
2023-02-18 10:44:30 -0500

    refactorización de vistas

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 from . import views
-from .views import Tramite_view
+from .views import Transaccion_view
 
 app_name = 'transactions'
 
@@ -15,5 +15,5 @@ urlpatterns = [
     path('test/', views.test, name="test"),
 
     path('dashboard/', login_required(views.dashboard), name="dashboard"),
-    path('tramite/', login_required(Tramite_view.as_view()), name='tramite')
+    path('tramite/', login_required(Transaccion_view.as_view()), name='transaccion')
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -1,114 +1,20 @@
 import json
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.http import HttpRequest, JsonResponse, HttpResponseBadRequest, QueryDict
+from django.http import HttpRequest, JsonResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.views.generic import View
 from django.urls import reverse
 
-from .models import Derecho, Tramite, Cliente, Transaccion, Vehiculo
+from .models import Tramite, Cliente, Vehiculo
 from .form import TransaccionForm, TramiteForm, ClienteForm, VehiculoForm
-from .serializers import TramiteSerializer, ClienteSerializer
-from django.core.exceptions import ValidationError
 from .utils import checker, buscar_objeto, search_in_model
 
 
-
 def test(request):
-    if request.method == "GET":
-        form = TramiteForm()
-        return render(request, 'test.html', {"form": form})
-
-    elif "buscar-cliente" == request.headers.get("X-Requested-Type"):
-        json_data = json.loads(request.body.decode("utf-8"))
-
-        num_documento_keyName = "num_documento"
-        tipo_documento_keyName = "tipo_documento"
-
-        is_num_documento_exists = True if json_data.get(
-            num_documento_keyName) else False
-        is_tipo_documento_exists = True if json_data.get(
-            tipo_documento_keyName) else False
-
-        if is_num_documento_exists and is_tipo_documento_exists:
-            num_documento = json_data[num_documento_keyName]
-            tipo_documento = json_data[tipo_documento_keyName]
-
-            if num_documento.isnumeric():
-                clientes = Cliente.objects.filter(
-                    num_documento=num_documento, tipo_documento=tipo_documento)
-                return JsonResponse(list(clientes.values()), safe=False)
-        return JsonResponse([], safe=False)
-    
-    elif "buscar-vehiculo" == request.headers.get("X-Requested-Type"):
-        json_data = json.loads(request.body.decode("utf-8"))
-        fields=["tipo_vehiculo", "placa"]
-        vehiculo= search_in_model(json_data, fields, Vehiculo)
-        return JsonResponse(vehiculo, safe=False)
-
-    elif "create_user" == request.headers.get("X-Requested-Type"):
-        querydict = QueryDict(request.body)
-        cliente = ClienteForm(querydict)
-        if cliente.is_valid():
-            cliente = cliente.save()
-            serializer = ClienteSerializer(cliente)
-            return JsonResponse(serializer.data, safe=False)
-        return JsonResponse([], safe=False)
-
-    elif "procesar-transaccion" == request.headers.get("X-Requested-Type"):
-        # FIXME: añadir un limitar de cuantos transsaciones puede hacer un cliente.
-        errors={}
-        data = json.loads(request.body)
-        
-        cliente= checker(data, ClienteForm)        
-        if isinstance(cliente, dict):
-            errors.update(cliente)
-        
-        if len(errors.keys())  >0:  
-            errors.update({"status":"fail"})          
-            return JsonResponse(errors, safe=False)        
-        
-        if cliente.pk is None:
-            cliente.save()  
-      
-        data["cliente"]=cliente
-        data["vehiculo"]= buscar_objeto(Vehiculo, data.get("vehiculo"))
-        data["tramite"]= buscar_objeto(Tramite, data.get("tramite"))
-        data["valor_total"]= "1"     
-
-        transaction = TransaccionForm(data=data)
-        if transaction.is_valid():
-            transaction.save()
-            return JsonResponse({"status":"ok","redirect_url": reverse("transactions:test")})
-        errors.update({"transaccion": transaction.errors.as_text(),"status":"fail"})
-        return JsonResponse(errors, safe=False)       
-        
-
-    elif "pre-select" in request.POST:
-        registro_name = 'registro'
-        clasificacion_name = 'clasificacion'
-
-        initial_data = {
-            registro_name: request.POST.get(registro_name),
-            clasificacion_name: request.POST.get(clasificacion_name)
-        }
-
-        tramite = TramiteForm(initial=initial_data)
-
-        tramite.fields[registro_name].widget.attrs['disabled'] = True
-        tramite.fields[clasificacion_name].widget.attrs['disabled'] = True
-        transaccion = TransaccionForm()
-        cliente = ClienteForm()
-        vehiculo= VehiculoForm()
-        context={
-            "transaccion": transaccion, 
-            "tramite": tramite, 
-            "cliente": cliente,
-            "vehiculo":vehiculo
-        }
-        return render(request, 'test-2.html', context)
+    return JsonResponse([], safe=False)
 
 
 def signout(request: HttpRequest):
@@ -159,42 +65,74 @@ def index(request):
     return render(request, "index.html")
 
 
-class Tramite_view(View):
+class Transaccion_view(View):
     def get(self, request):
-        form = TramiteForm()
-        return render(request, 'dashboard/test.html', {"form": form})
+        tramite = TramiteForm()       
+        cliente = ClienteForm()
+        vehiculo= VehiculoForm()
+        transaccion = TransaccionForm()
+        context={
+            "transaccion": transaccion, 
+            "tramite": tramite, 
+            "cliente": cliente,
+            "vehiculo":vehiculo
+        }
+        return render(request, 'dashboard/transaccion.html', context)
 
     def post(self, request):
-        if request.headers.get("X-Requested-Type") == "autocomplete":
+        if "buscar-cliente" == request.headers.get("X-Requested-Type"):
             json_data = json.loads(request.body.decode("utf-8"))
 
-            products = Derecho.objects.filter(
-                classification__icontains=json_data["classification"])
-            response_json = []
-            for product in products:
-                response_json.append(
-                    {
-                        "id": product.pk,
-                        "value": f"{product.name} - ${product.sale_value}",
-                        "name": product.name,
-                        "classification": product.classification,
-                        "percentage": round(product.percentage),
-                        "sale_value": product.sale_value
-                    }
-                )
-            return JsonResponse(response_json, safe=False)
+            num_documento_keyName = "num_documento"
+            tipo_documento_keyName = "tipo_documento"
 
-        elif request.headers.get("X-Requested-Type") == "post":
-            # TODO: Si alguien hace una petición post enviando los datos adecuados, pero con id de Derechos no existente ¿que ocurre?
-            data = json.loads(request.body)["tramite"]
+            is_num_documento_exists = True if json_data.get(
+                num_documento_keyName) else False
+            is_tipo_documento_exists = True if json_data.get(
+                tipo_documento_keyName) else False
 
-            derechos_id: list = data["derechos"]
-            derechosSer = DerechoSerializer(
-                Derecho.objects.filter(id__in=derechos_id), many=True)
-            data["derechos"] = derechosSer.data
+            if is_num_documento_exists and is_tipo_documento_exists:
+                num_documento = json_data[num_documento_keyName]
+                tipo_documento = json_data[tipo_documento_keyName]
 
-            tramite = TramiteSerializer(Tramite, data=data)
-            if tramite.is_valid():
-                tramite.save()
-                return JsonResponse({"redirect_url": reverse("transactions:tramite")})
-        return HttpResponseBadRequest("Invalid request")
+                if num_documento.isnumeric():
+                    clientes = Cliente.objects.filter(
+                        num_documento=num_documento, tipo_documento=tipo_documento)
+                    return JsonResponse(list(clientes.values()), safe=False)
+            return JsonResponse([], safe=False)
+        
+        elif "buscar-vehiculo" == request.headers.get("X-Requested-Type"):
+            json_data = json.loads(request.body.decode("utf-8"))
+            fields=["tipo_vehiculo", "placa"]
+            vehiculo= search_in_model(json_data, fields, Vehiculo)
+            return JsonResponse(vehiculo, safe=False)
+
+        elif "procesar-transaccion" == request.headers.get("X-Requested-Type"):
+            # FIXME: añadir un limitar de cuantos transsaciones puede hacer un cliente.
+            errors={}
+            data = json.loads(request.body)
+            
+            cliente= checker(data, ClienteForm)        
+            if isinstance(cliente, dict):
+                errors.update(cliente)
+            
+            if len(errors.keys())  >0:  
+                errors.update({"status":"fail"})          
+                return JsonResponse(errors, safe=False)        
+            
+            if cliente.pk is None:
+                cliente.save()  
+        
+            data["cliente"]=cliente
+            data["vehiculo"]= buscar_objeto(Vehiculo, data.get("vehiculo"))
+            data["tramite"]= buscar_objeto(Tramite, data.get("tramite"))
+            data["valor_total"]= "1"     
+
+            transaction = TransaccionForm(data=data)
+            if transaction.is_valid():
+                transaction.save()
+                return JsonResponse({"status":"ok","redirect_url": reverse("transactions:test")})
+            errors.update({"transaccion": transaction.errors.as_text(),"status":"fail"})
+            return JsonResponse(errors, safe=False)       
+            
+        return JsonResponse({'status':'false'}, status=400)
